b051df5d6513d94d1456fe83cb7cb7ede02d68c7
CN_syt22 <CN_syt22@noreply.gitcode.com>
2025-12-07 00:08:08 +0800

    lab4-2023113468-dev version4.0

--- src/test/java/L2023113468_18_Test.java
+++ src/test/java/L2023113468_18_Test.java
@@ -10,7 +10,6 @@ public class L2023113468_18_Test {
 
         int[] nums = {1,2,3,4,5,6};
         int[] expected = {720, 360, 240, 180, 144, 120};// correct//
-        int[] meaningless = {1, 1, 1, 1, 1, 1};
         //int[] expected = {120, 60, 40, 30, 24, 20}; // wrong
         int[] result = s.productExceptSelf(nums);
 
